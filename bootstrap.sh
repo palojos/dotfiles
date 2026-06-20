9821b0e0cb16780e3cc32a3d62400d476d8ab445
Oskari Palojärvi <oskari@palojarvi.fi>
2026-06-20 19:30:19 +0300

    Tmux continuum setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-set -e
+set -euo pipefail
 
 rsync -av local/ ~/.local/
 rsync -av config/ ~/.config/
 rsync -av home/ ~/
+
+
+TMUX_PLUGIN_DIR=$HOME/.tmux/plugins/tpm 
+
+# Install tmux plugin manager
+if [ ! -d "$TMUX_PLUGIN_DIR" ]; then
+    git clone --depth 1 https://github.com/tmux-plugins/tpm "$TMUX_PLUGIN_DIR"
+fi
